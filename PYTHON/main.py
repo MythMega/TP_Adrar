b6ea3df90b030307d0ddbbdd0422b0efc1c6cf3d
MythMega <45936113+MythMega@users.noreply.github.com>
2021-12-21 10:32:45 +0100

    Finito Team Aleatoire

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,8 +1,18 @@
+from random import *
+import time
 
-SCORE_DE_BASE = 20
-
+match_termine = False
+SCORE_DE_BASE = 5
 les_scores = {}
+les_equipes = []
 eq_alias = {}
+
+def displayScore(mes_scores):
+    for i in range(5): print(" ")
+    print("-LES SCORES SONT LES SUIVANTS-")
+    for cle, valeur in mes_scores.items():
+        print("Pour l'équipe ", cle, " : ", valeur, " points restants")
+    time.sleep(1)
 
 for _ in range(2):
     tout_est_good = False
@@ -15,6 +25,35 @@ for _ in range(2):
         else:
             print("----------------------\n-----C'est validé-----\n----------------------")
             tout_est_good = True
-    les_scores[une_equipe] = 20
+    les_scores[une_equipe] = SCORE_DE_BASE
+    les_equipes.append(une_equipe)
+time.sleep(1)
+for i in range(5): print(" ")
+print("debut du match !\n----------------")
 
+while not match_termine:
+    #####Selection de qui va être le perdant
+    displayScore(les_scores)
+    aleatoire = randint(0,1)
+    loser = les_equipes[aleatoire]
+    les_scores[loser] -= 1
+    for i in les_scores:
+        if les_scores[i] < 1:
+            match_termine = True
 
+for i in range(10): print(" ")
+print("Les scores finaux sont tombés !")
+time.sleep(1)
+displayScore(les_scores)
+loser = ""
+winner = ""
+for i, j in les_scores.items():
+    if j == 0:
+        loser = i
+    if j != 0:
+        winner = i
+
+for i in range(10): print(" ")
+print("--------------------------------------")
+print(f"Les vainqueurs sont donc les {winner}")
+print("--------------------------------------")
